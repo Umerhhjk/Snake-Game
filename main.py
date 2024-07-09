40dcfc2a05430b74fe227c2757aca831116da065
umerhhjk <umertaiyab@gmail.com>
2024-07-09 17:35:49 +0500

    setup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 
 from food import Food
+from scoreboard import ScoreBoard
 from snake import Snake
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
